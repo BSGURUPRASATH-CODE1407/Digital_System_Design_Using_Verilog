564002faa36f7bbde9d8209cbff345febfe6a0d7
BSGURUPRASATH-CODE1407 <guruprasath.1407@gmail.com>
2026-06-20 15:56:26 +0530

    Create testbench for decimal to binary conversion
    Added a testbench for the decimal to binary conversion module, simulating inputs from 0 to 9 and displaying the results.

--- UNIT-I Basic_Concepts/1.Number_Systems/2.Decimal_Number_System_and_its_Conversions/Decimal_to_Binary_conversion/dectobin_tb.v
+++ UNIT-I Basic_Concepts/1.Number_Systems/2.Decimal_Number_System_and_its_Conversions/Decimal_to_Binary_conversion/dectobin_tb.v
@@ -1,1 +1,16 @@
 
+`timescale 1ns/1ps
+module dectobin_tb;
+    reg [7:0] decimal_in;
+    wire [7:0] binary_out;
+    integer i;
+    dectobin uut(.decimal_in(decimal_in),.binary_out(binary_out));
+    initial begin
+       for(i = 0; i <= 9; i = i + 1) 
+        begin
+            decimal_in =i;#10;
+            $display("Decimal= %d Binary= %b",decimal_in,binary_out);
+        end
+       $finish;
+    end
+endmodule
