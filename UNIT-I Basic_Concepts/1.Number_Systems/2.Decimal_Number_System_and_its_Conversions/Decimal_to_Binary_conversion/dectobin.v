4ad3499607c02d6a9b028a6ff3e2a1f6c9f077bc
BSGURUPRASATH-CODE1407 <guruprasath.1407@gmail.com>
2026-06-20 16:20:52 +0530

    Adjust input and output bit width for dectobin module

--- UNIT-I Basic_Concepts/1.Number_Systems/2.Decimal_Number_System_and_its_Conversions/Decimal_to_Binary_conversion/dectobin.v
+++ UNIT-I Basic_Concepts/1.Number_Systems/2.Decimal_Number_System_and_its_Conversions/Decimal_to_Binary_conversion/dectobin.v
@@ -1,8 +1,8 @@
 
 `timescale 1ns / 1ps
 module dectooct(
-    input [7:0] decimal_in,
-    output [7:0] octal_out
+    input [3:0] decimal_in,
+    output [3:0] octal_out
     );
     assign octal_out=decimal_in;
 endmodule
