1aa7788199c93fe0ee5f6f9428613f28e6a4a1ee
BSGURUPRASATH-CODE1407 <guruprasath.1407@gmail.com>
2026-06-25 22:51:19 +0530

    Update bintooct module for binary to octal conversion
    Refactor binary to octal conversion module and remove testbench.

--- UNIT-I Basic_Concepts/1.Number_Systems/1.Binary_Number_Systems_and _its_Conversions/Binary_to_Octal_conversion/bintooct.v
+++ UNIT-I Basic_Concepts/1.Number_Systems/1.Binary_Number_Systems_and _its_Conversions/Binary_to_Octal_conversion/bintooct.v
@@ -1,15 +1,12 @@
 `timescale 1ns / 1ps
-module bintooct_tb;
-    reg [7:0] binary_in;
-    wire [11:0] octal_out;
-    bintooct uut(.binary_in(binary_in),.octal_out(octal_out));
-    integer i;
-    initial begin
-      for(i=0;i<=15;i=i+1)
-      begin
-        binary_in=i;#10;
-        $display ("Binary= %b Octal= %0d%0d%0d",binary_in,octal_out[11:8],octal_out[7:4],octal_out[3:0]);
-      end
-      $finish;
+module bintooct(
+    input [7:0] binary_in,
+    output reg [11:0] octal_out
+    );
+    always @(*)
+    begin
+       octal_out[11:8]=binary_in/64;
+      octal_out[7:4]=(binary_in%64)/8;
+      octal_out[3:0]=binary_in%8;
     end
 endmodule
