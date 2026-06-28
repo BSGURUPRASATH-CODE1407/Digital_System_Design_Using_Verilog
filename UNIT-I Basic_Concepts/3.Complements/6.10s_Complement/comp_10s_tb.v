49685aeb61c9825cb2aeef33a00c53898fe65494
BSGURUPRASATH-CODE1407 <guruprasath.1407@gmail.com>
2026-06-28 13:21:13 +0530

    Create comp_10s_tb.v

---
+++ UNIT-I Basic_Concepts/3.Complements/6.10s_Complement/comp_10s_tb.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module comp_10s_tb;
+    reg [3:0] in;
+    wire [3:0] out;
+    comp_10s uut(.in(in),.out(out));
+    integer i;
+    initial
+    begin
+      for(i=0;i<=9;i=i+1)
+      begin
+        in=i;#10;
+        $display("Input = %d Output = %d",in,out);
+      end
+      $finish;
+    end
+endmodule
